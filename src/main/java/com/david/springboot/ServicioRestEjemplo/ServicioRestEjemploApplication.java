0f6c8f8ecc62e6bc3e9653fcfe0f5fe31421ef92
David G <dgomgra@gmail.com>
2018-07-03 18:15:51 +0200

    Se añaden las dependencias de spring.

---
+++ src/main/java/com/david/springboot/ServicioRestEjemplo/ServicioRestEjemploApplication.java
@@ -0,0 +1,12 @@
+package com.david.springboot.ServicioRestEjemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRestEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioRestEjemploApplication.class, args);
+	}
+}
